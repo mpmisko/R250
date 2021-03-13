25e137b58c4fa55b2ceefbc92aa8a844613426f9
mp5217 <michal.pandy17@imperial.ac.uk>
2021-03-13 23:36:01 +0000

    Ablate target intervals

--- sac_d/train.py
+++ sac_d/train.py
@@ -152,6 +152,7 @@ def run_sacd_hyperparam_eval(env, num_episodes, sacd_config, log_dir, path='./ab
         'memory_size' : [200, 2000, 20000],
         'target_entropy_ratio' : [0.9, 0.95, 0.99],
         'use_per' : [True, False],
+        'target_update_interval' : [40, 400, 4000]
     }
     
     # Create logging file
@@ -162,27 +163,29 @@ def run_sacd_hyperparam_eval(env, num_episodes, sacd_config, log_dir, path='./ab
     for memory_size in hyperparams['memory_size']:
         for ratio in hyperparams['target_entropy_ratio']:
             for per in hyperparams['use_per']:
+                for update_interval in hyperparams['target_update_interval']:
                     params = {
                         'memory_size' : memory_size,
                         'target_entropy_ratio' : ratio,
                         'use_per' : per,
+                        'target_update_interval': update_interval
                     }
 
                     agent1, agent2 = get_agents(env, env, log_dir, sacd_config, sacd_config, params)
-                    print(f"Starting traning hyperparam config: [memory] -- {memory_size}, [target_entropy_ratio] -- {ratio}, [use_per] -- {int(per)}")
+                    print(f"Starting traning hyperparam config: [memory] -- {memory_size}, [target_update_interval] -- {update_interval}, [target_entropy_ratio] -- {ratio}, [use_per] -- {int(per)}")
                     run_train(env, agent1, agent2, num_episodes)
 
 
                     # Log agent 1
                     with open(path, 'a') as f: 
                         for episode, val in enumerate(agent1.train_returns):
-                            data = f"{memory_size},{ratio},{int(per)},{episode+1},{1},{val}\n"
+                            data = f"{memory_size},{ratio},{int(per)},{update_interval},{episode+1},{1},{val}\n"
                             f.write(data)
 
                     # Log agent 2
                     with open(path, 'a') as f: 
                         for episode, val in enumerate(agent2.train_returns):
-                            data = f"{memory_size},{ratio},{int(per)},{episode+1},{2},{val}\n"
+                            data = f"{memory_size},{ratio},{int(per)},{update_interval},{episode+1},{2},{val}\n"
                             f.write(data)
 
 
